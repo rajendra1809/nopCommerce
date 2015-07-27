346abb0b585bb465a340fb9cae35b675e5860722
Dmitriy <dmitriy.khomych@gmail.com>
2015-07-27 15:55:24 +0300

    Initial commit

---
+++ Presentation/Nop.Web/Administration/Models/Customers/CustomerAddressModel.cs
@@ -0,0 +1,13 @@
+﻿using Nop.Admin.Models.Common;
+using Nop.Web.Framework.Mvc;
+
+namespace Nop.Admin.Models.Customers
+{
+    public partial class CustomerAddressModel : BaseNopModel
+    {
+        public int CustomerId { get; set; }
+
+        public int CustomerAge { get; set; }
+        public AddressModel Address { get; set; }
+    }
+}
